25c9487809fbe908f7c7cb1212fe9a49ce38f68e
Joabalonzo <Joabalonzo>
2026-09-12 10:32:40 +0800

    done 5%

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+inp_folder = input("Enter Folde Path : ")
+
+os.path.exists(inp_folder)
+
+os.listdir("Test_JoabAlonzo")
+
+
+    
+#if input == "Images" or "Others" or "Documents" or "Videos":
+                      #continue
+    #elif
+#list_of_files = os.listdir("Test_JoabAlonzo")
+#print(list_of_files)
+#for input in inp_folder:
